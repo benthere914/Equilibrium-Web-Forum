60ad0e26c65a1dc2834aeda228877a453f4a5560
Benjamin Rose <benthere914@gmail.com>
2021-09-21 14:04:53 -0500

    made the basic structure and basic styling for the post page, along with the endpoint for the post page

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+
 
 const app = express();
 
@@ -38,6 +40,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
