965344f0db1077aaed6aab0b9951a481d20bdcbe
Parker Bolick <bolickparker@gmail.com>
2021-09-21 21:17:59 -0400

    Added vote routing - IN PROGRESS

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,8 @@ router.get('/', function(req, res, next) {
 router.get("/userid", (req, res) => {
   if (req.session.auth.userId){
     res.json({userId: req.session.auth.userId})
-  } else
-  {
-    res.json({userId: NaN})
+  } else {
+    res.json({userId: NaN});
     }
   })
 
