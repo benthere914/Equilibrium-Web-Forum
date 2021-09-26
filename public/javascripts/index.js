5ede94c20f66a6247e552cb1148071df7c3100dd
nick <nyfrase3@live.com>
2021-09-26 17:14:17 -0400

    somehow someway

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,96 +1,96 @@
-
-
-
 window.addEventListener("load", async (event) => {
-    document.querySelector(".nav-bar").scrollIntoView();
+	document.querySelector(".nav-bar").scrollIntoView();
 
-	let current = 50;
+let current = 50;
 
-	let loggedIn = false;
-	try {
-		let userId = await getData("/users/userid");
-		userId = userId.userId;
+let loggedIn = false;
+try {
+	let userId = await getData("/users/userid");
+	userId = userId.userId;
 
-		if (typeof userId === "number") {
-			loggedIn = true;
+	if (typeof userId === "number") {
+		loggedIn = true;
 
-			let followsTopics = await getData(`follows/topics/${userId}`);
-			followsTopics = followsTopics.map((each) => {
-				return `topic-${each.topicId}`;
-			});
-			let allTopics = document.querySelectorAll(".topics-only .topics");
-			allTopics.forEach(topic => {
-				topic.classList.add("loggedIn");
-			})
-			let nodes = [];
-			followsTopics.forEach((x) => {
-				nodes.push(document.getElementById(`${x}`));
-			});
-			nodes.forEach((node) => {
-				node.classList.add("toggled");
-				node.style.order = 1;
-			});
-			let relevantUrls= [];
-			for (let each of followsTopics){
-				relevantUrls.push(`relevant-${each.split('-')[1]}`)
-			}
-				let intiialRelevantNodes = [];
-			for (let i = 0; i < relevantUrls.length; i++){
-				let posts = document.querySelectorAll(`.${relevantUrls[i]}`);
-				if (posts.length){
-					posts.forEach(post => post.style.order = current -1);
-				}
-			}
+		let followsTopics = await getData(`follows/topics/${userId}`);
+		followsTopics = followsTopics.map((each) => {
+			return `topic-${each.topicId}`;
+		});
+		let allTopics = document.querySelectorAll(".topics-only .topics");
+		allTopics.forEach(topic => {
+			topic.classList.add("loggedIn");
+		})
+		let nodes = [];
+		followsTopics.forEach((x) => {
+			nodes.push(document.getElementById(`${x}`));
+		});
+		nodes.forEach((node) => {
+			node.classList.add("toggled");
+			node.style.order = 1;
+		});
+		let relevantUrls= [];
+		for (let each of followsTopics){
+			relevantUrls.push(`relevant-${each.split('-')[1]}`)
 		}
-	} catch (e) {}
+			let intiialRelevantNodes = [];
+		for (let i = 0; i < relevantUrls.length; i++){
+			let posts = document.querySelectorAll(`.${relevantUrls[i]}`);
+			if (posts.length){
+				posts.forEach(post => post.style.order = current -1);
+			}
+		}	if(relevantUrls.length){current--};
+	}
+} catch (e) {}
 
-	const topics = document.querySelectorAll(".topics").forEach((topic) => {
-		topic.addEventListener("click", async (e) => {
-			const topicId = topic.id.split("-")[1];
-			if (!e.target.classList.contains("toggled")) {
-				toggle(e.target);
-				e.target.style.order = 1;
-				let cssId = `relevant-${topicId}`;
+const topics = document.querySelectorAll(".topics").forEach((topic) => {
+	topic.addEventListener("click", async (e) => {
+		const topicId = topic.id.split("-")[1];
+		if (!e.target.classList.contains("toggled")) {
+			toggle(e.target);
+			e.target.style.order = 1;
+			let cssId = `relevant-${topicId}`;
 
-				const relevantPosts = document.querySelectorAll(`.${cssId}`);
-				relevantPosts.forEach((post) => {
-					post.style.order = current;
-					current--;
-				});
-
-				if (loggedIn) {
-					let userId = await getData("/users/userid");
-					const body = {
-						userId: userId.userId,
-						topicId: parseInt(topicId, 10),
-					};
-					let res = await fetch("/follows/topics", {
-						method: "POST",
-						headers: { "Content-Type": "application/json" },
-						body: JSON.stringify(body),
-					});
-				}
-			} else if (e.target.classList.contains("toggled")){
-				e.target.classList.remove('toggled');
-				e.target.style.order = 12;
-
-			}let cssId = `relevant-${topicId}`;
 			const relevantPosts = document.querySelectorAll(`.${cssId}`);
+
+			if (relevantPosts.length){ current = current -1};
 			relevantPosts.forEach((post) => {
 				post.style.order = current;
-				if (current < 50) current ++;
 			});
 
+			if (loggedIn) {
+				let userId = await getData("/users/userid");
+				const body = {
+					userId: userId.userId,
+					topicId: parseInt(topicId, 10),
+				};
+				let res = await fetch("/follows/topics", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(body),
+				});
+			}
+		} else if (e.target.classList.contains("toggled")){
+			e.target.classList.remove('toggled');
+			e.target.style.order = 12;
+
+			let cssId = `relevant-${topicId}`;
+		const relevantPosts = document.querySelectorAll(`.${cssId}`);
+		if (relevantPosts.length){if (current < 50) current ++}
+
+		relevantPosts.forEach((post) => {
+			post.style.order = current;
 		});
+		}
+
 	});
+});
 });
 
 let getData = async (url) => {
-	const response = await fetch(url);
+const response = await fetch(url);
 
-	return response.json();
+return response.json();
 }
 
- let toggle = (element) => {
-	element.classList.add("toggled");
+let toggle = (element) => {
+element.classList.add("toggled");
 };
