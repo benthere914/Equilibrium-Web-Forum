0ec01351947bc9370afbcd95ca16d5ade85e6ce0
Benjamin Rose <benthere914@gmail.com>
2021-09-26 20:01:17 -0500

    testing on main site

--- public/javascripts/remove-large-images.js
+++ public/javascripts/remove-large-images.js
@@ -1,8 +1,8 @@
-let images = document.querySelectorAll('.recommended ul li img');
-let deleteParentDiv = (e) => {
-    console.log(e.parentElement.parentElement)
-    e.parentElement.parentElement.remove()
-}
-images.forEach(e => {
-    console.log(e.clientWidth, e.clientHeight)
-    if (e.clientHeight > 200 || e.clientWidth > 200){deleteParentDiv}})
+// let images = document.querySelectorAll('.recommended ul li img');
+// let deleteParentDiv = (e) => {
+//     console.log(e.parentElement.parentElement)
+//     e.parentElement.parentElement.remove()
+// }
+// images.forEach(e => {
+//     console.log(e.clientWidth, e.clientHeight)
+//     if (e.clientHeight > 200 || e.clientWidth > 200){deleteParentDiv}})
