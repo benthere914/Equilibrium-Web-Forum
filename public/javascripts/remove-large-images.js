51acd90d4d3acf7c998623de954684382faf4dc1
Benjamin Rose <benthere914@gmail.com>
2021-09-26 20:03:52 -0500

    removed code

--- public/javascripts/remove-large-images.js
+++
@@ -1,8 +0,0 @@
-// let images = document.querySelectorAll('.recommended ul li img');
-// let deleteParentDiv = (e) => {
-//     console.log(e.parentElement.parentElement)
-//     e.parentElement.parentElement.remove()
-// }
-// images.forEach(e => {
-//     console.log(e.clientWidth, e.clientHeight)
-//     if (e.clientHeight > 200 || e.clientWidth > 200){deleteParentDiv}})
